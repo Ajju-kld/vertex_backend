3779e530898a5e07c046ab2059787cad17fe5902
Ajju-kld <mohammedajmal996@gmail.com>
2024-06-11 18:05:09 +0530

    chore: Add getSelfProfile route and verifyToken middleware to auth.routes.mjs

--- routes/auth.routes.mjs
+++ routes/auth.routes.mjs
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, Register } from "../controllers/auth.controller.mjs";
+import { getSelfProfile, login, Register } from "../controllers/auth.controller.mjs";
+import { verifyToken } from "../middlewares/auth.middleware.mjs";
 const router = Router();
 
 router.post("/register",Register );
 router.post("/login", login);
+router.get("/self",verifyToken,getSelfProfile)
 
 
 export default router;
