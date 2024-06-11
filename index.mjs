beaa95d69504d3d21239bda28000839b8fa2e2cc
Ajju-kld <mohammedajmal996@gmail.com>
2024-06-11 18:25:33 +0530

    chore: Add cors middleware for handling cross-origin requests

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors";
 import {Server} from 'socket.io';
 import http from 'http';
 import promise from "./config/db.mjs";
@@ -12,25 +12,27 @@ import interractionRoutes from "./routes/interaction.routes.mjs";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+app.use(cors());
+app.use(express.json());
 app.use(requestLogger);
 app.use(errorHandler);
-app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.get('/api', (req, res) => {
-    res.send( 'development server is running fine on api/!');
-});
-
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  res.send( 'development server is running fine on api/!');
   });
-});
-// routes
-app.use("/api/auth",authRoutes);
-app.use("/api/post",verifyToken,postRoutes);
-app.use("/api/comment",verifyToken,commentRoutes);
-app.use("/interaction",verifyToken,interractionRoutes);
-
-
-server.listen(5000, () => {
-    console.log('the server listening on :5000');
-});
+  
+  io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+      io.emit("chat message", msg);
+      });
+      });
+      // routes
+      app.use("/api/auth",authRoutes);
+      app.use("/api/post",verifyToken,postRoutes);
+      app.use("/api/comment",verifyToken,commentRoutes);
+      app.use("/interaction",verifyToken,interractionRoutes);
+      
+      
+      server.listen(5000, () => {
+        console.log('the server listening on :5000');
+        });
