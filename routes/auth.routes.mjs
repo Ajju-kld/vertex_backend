d344b6a9fd186a1d7e47fd75cba80d8f014f70a2
Ajju-kld <mohammedajmal996@gmail.com>
2024-07-18 23:06:48 +0530

    feat: Add endpoint for updating user profile picture

--- routes/auth.routes.mjs
+++ routes/auth.routes.mjs
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getSelfProfile, login, Register } from "../controllers/auth.controller.mjs";
+import { getSelfProfile, login, Register,uploadProfile } from "../controllers/auth.controller.mjs";
 import { verifyToken } from "../middlewares/auth.middleware.mjs";
 const router = Router();
 
 router.post("/register",Register );
 router.post("/login", login);
 router.get("/self",verifyToken,getSelfProfile)
-
+router.put('/setProfile',verifyToken,uploadProfile)
 
 export default router;
 // Path: routes/post.routes.mjs
