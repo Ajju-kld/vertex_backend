6dd97717c4b63bb48a1c5100bcf1bb4d9216cf98
Ajju-kld <mohammedajmal996@gmail.com>
2024-07-20 13:13:18 +0530

    refactor: Update post controller to use uploadPost function for post upload functionality

--- routes/post.routes.mjs
+++ routes/post.routes.mjs
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { verifyToken } from "../middlewares/auth.middleware.mjs";
-import { deletePost, getPostbyId, likePost,  uploadPostContent, uploadPostDetails } from "../controllers/post.controller.mjs";
+import { deletePost, getPostbyId, likePost, uploadPost } from "../controllers/post.controller.mjs";
 import { upload } from "../middlewares/upload.middleware.mjs";
 
 
