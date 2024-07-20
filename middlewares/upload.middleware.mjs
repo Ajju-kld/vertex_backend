c18df9e035217c8e869b75d89c9fef2d59647626
Ajju-kld <mohammedajmal996@gmail.com>
2024-07-20 14:40:10 +0530

    refactor: Update upload.middleware to use fs/promises for file system operations

--- middlewares/upload.middleware.mjs
+++ middlewares/upload.middleware.mjs
@@ -7,7 +7,7 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-import fs from "fs";
+import fs from "fs/promises";
 import {
   DO_SPACES_BUCKET,
   DO_SPACES_KEY,
