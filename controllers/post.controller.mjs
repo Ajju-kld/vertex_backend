ef024e8abbf506477c254dcb9fb73ff57a87f041
Ajju-kld <mohammedajmal996@gmail.com>
2024-07-26 10:06:27 +0530

    refactor: Update post.controller to pass posts data to worker thread

--- controllers/post.controller.mjs
+++ controllers/post.controller.mjs
@@ -246,7 +246,7 @@ const getallposts = async (req, res, next) => {
       .populate("user", "-passwordHash -followers -following -_id -email")
       .sort("-createdAt");
 
-    const worker = new Worker("./worker/postWorker.js",workerData(posts));
+    const worker = new Worker("./worker/postWorker.js",{ workerData: posts });
     console.log("worker:", worker);
 
 
