0bcca32f3691b77a4dd43ce4df21ca0261a3ed05
Ajju-kld <mohammedajmal996@gmail.com>
2024-07-26 11:48:51 +0530

    refactor: Update post.controller to remove commented out code and unused variables

--- controllers/post.controller.mjs
+++ controllers/post.controller.mjs
@@ -245,8 +245,8 @@ const getallposts = async (req, res, next) => {
     const posts = await Post.find()
       .populate("user", "-passwordHash -followers -following -_id -email")
       .sort("-createdAt");
-          const worker = new Worker("./postWorker.js", { workerData: posts });
-          console.log("worker:", worker);
+          // const worker = new Worker("./postWorker.js", { workerData: posts });
+          // console.log("worker:", worker);
       const Posts = await Post.aggregate([
         {
           $lookup: {
@@ -284,19 +284,23 @@ const getallposts = async (req, res, next) => {
 
     
 
-    worker.on("message", (result) => {
-      console.log("result:", result);
-      res.status(200).json({
-        message: "Posts fetched successfully",
-        posts: result.filteredPosts,
-        postIds: result.postIds,
-      });
-    });
+    // worker.on("message", (result) => {
+    //   console.log("result:", result);
+    //   res.status(200).json({
+    //     message: "Posts fetched successfully",
+    //     posts: result.filteredPosts,
+    //     postIds: result.postIds,
+    //   });
+    // });
 
-    worker.on("error", (error) => {
-      console.error("Error in worker:", error);
-      next(error);
-    });
+res.status(200).json({
+  message: "Posts fetched successfully",
+  posts: Posts,
+});
+    // worker.on("error", (error) => {
+    //   console.error("Error in worker:", error);
+    //   next(error);
+    // });
   } catch (error) {
     console.error("Error in getallposts:", error);
     next(error);
